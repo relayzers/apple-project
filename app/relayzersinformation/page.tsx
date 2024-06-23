35dabae60b3d3a36b36a34543cd3abe53973f079
relayzers <relayzers@mail.ru>
2024-06-23 17:27:47 +0400

    updated 4

--- app/relayzersinformation/page.tsx
+++ app/relayzersinformation/page.tsx
@@ -8,40 +8,40 @@ import { IoMdMail } from 'react-icons/io';
 
 const page = () => {
     return (
-        <div>
+        <div className=''>
             <div className='w-full h-12 bg-black'>
             </div>
             <p className='text-4xl font-bold  text-center mt-32 border-2 border-black p-5 transition-all w-fit mx-auto hover:bg-slate-300'>r e l a y z e r s</p>
             <p className='text-3xl font-semibold text-center mt-5'>Communication</p>
-            <Grid container justifyContent={'space-around'} marginY={10}>
-                <Box width={200} height={200}>
+            <Grid container justifyContent={'space-around'} marginY={10} marginRight={10} spacing={4}>
+                <Grid width={200} height={200} item xs={12} sm={6} md={4} lg={2} xl={1} >
                     <div className='rounded-3xl shadow-2xl transition-all hover:shadow-green-400'>
-                        <Link href="tel:+994706789125"><FaWhatsapp className='p-14 mx-10 text-9xl transition-all hover:p-10  ' /></Link>
+                        <Link href="tel:+994706789125"><FaWhatsapp className='p-14  text-9xl transition-all hover:p-10  ' /></Link>
                     </div>
-                </Box>
-                <Box width={200} height={200}>
+                </Grid>
+                <Grid width={200} height={200} item xs={12} sm={6} md={4} lg={2} xl={1} >
                     <div className=' rounded-3xl shadow-2xl transition-all hover:shadow-pink-600'>
                         <Link target='_blank' href="https://www.instagram.com/relayzers/"><FaInstagram className='p-14 mx-10 text-9xl transition-all hover:p-10' /></Link>
                     </div>
-                </Box>
-                <Box width={200} height={200}>
+                </Grid>
+                <Grid width={200} height={200} item xs={12} sm={6} md={4} lg={2} xl={1} >
                     <div className=' rounded-3xl shadow-2xl transition-all hover:shadow-black'>
                         <Link target='_blank' href="https://www.x.com/relayzers"><FaXTwitter className='p-14 mx-10 text-9xl transition-all hover:p-10' /></Link>
                     </div>
-                </Box>
-                <Box width={200} height={200}>
+                </Grid>
+                <Grid width={200} height={200} item xs={12} sm={6} md={4} lg={2} xl={1} >
                     <div className=' rounded-3xl shadow-2xl transition-all hover:shadow-sky-500'>
                         <Link target='_blank' href="https://www.linkedin.com/in/relayzers/"><FaLinkedin className='p-14 mx-10 text-9xl transition-all hover:p-10' /></Link>
                     </div>
-                </Box>
-                <Box width={200} height={200}>
+                </Grid>
+                <Grid width={200} height={200} item xs={12} sm={6} md={4} lg={2} xl={1} >
                     <div className=' rounded-3xl shadow-2xl transition-all hover:shadow-red-400'>
                         <Link href="mailto:relayzers@mail.ru"><IoMdMail className='p-14 mx-10 text-9xl transition-all hover:p-10' /></Link>
                     </div>
-                </Box>
+                </Grid>
             </Grid>
-            <Typography className='text-4xl font-bold mt-10 text-center'><Link href="/"><Image src="/store/applelogoblack.svg" alt="" width={100} height={100} className='mx-auto' /></Link></Typography>
-            <Typography className='text-4xl font-bold my-20 text-center'>Thank You Support</Typography>
+            <Typography className='text-4xl font-bold mt-10 text-center border-2 rounded-full w-max px-8 p-4 mx-auto border-black'><Link href="/"><Image src="/store/applelogoblack.svg" alt="" width={100} height={100} className='mx-auto' /></Link></Typography>
+            <Typography className='text-4xl font-bold my-10 text-center'>Thank You Support</Typography>
 
         </div>
     )
--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Metadata } from "next";
-import { Box, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
+import Image from 'next/image';
 export const metadata: Metadata = {
   title: "Tv & Home",
 };
@@ -10,8 +11,76 @@ const Tv = () => {
     <div>
       <div className='w-full h-12 bg-black'>
       </div>
-      <div className="relative overflow-hidden bg-gray-100 py-4">
+      <Grid container className='flex justify-center gap-12 mt-10 h-fit text-center mx-auto' spacing={2}>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/appletv4k.svg" alt='photo' width={70} height={70} className='mt-0.5 mx-auto' />
+          <p className='mt-3.5'>Apple TV 4K</p>
+          <p className='text-orange-600 '>New</p>
+        </Grid>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/appletv.svg" alt='photo' width={50} height={70} className='mx-auto' />
+          <p className='mt-3.5'>Apple TV app</p>
+        </Grid>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/appletv+.svg" alt='photo' width={90} height={70} className='mt-2 mx-auto' />
+          <p className='mt-5'>Apple TV+</p>
+        </Grid>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/homepod.svg" alt='photo' width={50} height={70} className='mt-1 mx-auto' />
+          <p className='mt-3.5'>HomePod</p>
+        </Grid>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/homeapp.svg" alt='photo' width={55} height={70} className='mx-auto' />
+          <p className='mt-3.5'>Home app</p>
+        </Grid>
+        <Grid item xs={12} sm={5} md={3} lg={1} xl={1}>
+          <Image src="/tv/accessories.svg" alt='photo' width={30} height={70} className='mx-auto' />
+          <p className='mt-3.5'>Accessories</p>
+        </Grid>
+      </Grid>
+      <div className='bg-blue-600 py-4'>
+        <p className='text-white text-center text-md'>MLS Season Pass. Stream every Major League Soccer match. Coming soon.* Notify Me</p>
       </div>
+      <Image src="/tv/herotv.svg" alt='photo' width={1400} height={700} className='mx-auto mb-20' />
+      <Image src="/tv/herohomepod.svg" alt='photo' width={1400} height={700} className='mx-auto' />
+      <div>
+        <p className='text-center text-7xl font-bold -mt-10'>The future hits home.</p>
+        <p className='text-center text-xl mt-5 text-gray-500'>Simply connect your favorite devices and transform your house into a remarkably smart,<br /> convenient, and entertaining home. Control lights, locks, and thermostats with your <br /> iPhone — or just your voice. Play any song, in any room, from anywhere. And elevate movie <br /> night with theater-like picture and sound. All with the security and privacy of Apple.</p>
+        <p className='text-xs text-center mt-2 text-gray-500'>Stream Luck on the Apple TV app</p>
+      </div>
+      <Grid>
+        <Grid container justifyContent={'center'} marginTop={10} gap={10}>
+          <Grid >
+            <div className='text-center'>
+              <p className='text-lg font-bold my-3'>HomePod mini </p>
+              <p className='text-2xl font-bold'>Room-filling sound <br /> in every room.</p>
+              <p className='my-5 font-medium'>$99</p>
+              <Button variant='contained' className='rounded-full normal-case mr-5'>Buy</Button>
+              <Button className='normal-case'>Learn more</Button>
+            </div>
+            <Image src="/tv/fililing.svg" alt='photo' width={550} height={400} />
+          </Grid>
+          <Grid >
+            <div className='text-center'>
+              <p className='text-orange-600 -mt-4'>New</p>
+              <Image src="/tv/4klogo.svg" alt='photo' width={70} height={400} className='mx-auto my-3' />
+              <p className='text-2xl font-bold'>The Apple experience. <br /> Cinematic in every sense.</p>
+              <p className='my-5 font-medium'>Starting at $129</p>
+              <Button className='normal-case  rounded-full mr-5' variant='contained'>Buy</Button>
+              <Button className='normal-case'>Learn more</Button>
+            </div>
+            <Image src="/tv/4ktv.svg" alt='photo' width={550} height={400} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid>
+          <Image src="" alt='photo' width={500} height={500}/>
+        </Grid>
+        <Grid>
+
+        </Grid>
+      </Grid>
     </div >
   )
 }
